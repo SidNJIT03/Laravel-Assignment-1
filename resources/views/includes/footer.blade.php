41f01edbb654acbcc8d3e4116342326094d4aa6f
Siddharth Santhanakrishnan <ss3875@njit.edu>
2018-10-15 00:09:04 -0400

    Feature: Added includes file with head, header, footer and jsfooter

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="row mt-4 mb-4"  class="font-weight-light">
+    &copy; 2016-2018 Bluegrass, Inc. &middot; <a href="#"  class="font-weight-light">Privacy</a> &middot; <a href="#"  class="font-weight-light">Terms</a>
+</div>
